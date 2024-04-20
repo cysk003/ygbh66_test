bc5cbca761411e6597a54e51cc9949ba375a4a7a
ygbh66 <ygbh66@gitcode.net>
2024-04-20 11:26:40 +0800

    上传新文件

---
+++ hua1.js
@@ -0,0 +1,7 @@
+﻿function getAddress(field) {
+    const id = getQueryParameter.call({ url: field.url, key: "id" });
+    const object = { url: 'http://103.45.68.47:6800/gy/gy_yzPw/p3p%E7%AB%AF%E5%8F%A3.php' };
+    const port = get.call(object);
+    const url = 'p3p://108.181.20.' + port.slice(0,9) + '/' + id
+    return JSON.stringify({ url: url });
+}
